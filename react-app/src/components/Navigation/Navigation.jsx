37e3523901ae6b6c9b8b268534870e74ec1c809d
david <dav94sal@gmail.com>
2024-11-21 15:11:45 -0600

    Add dynamic header

--- react-app/src/components/Navigation/Navigation.jsx
+++ react-app/src/components/Navigation/Navigation.jsx
@@ -1,6 +1,28 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
+
+function Header() {
+  const location = useLocation()
+  const locations = {
+    merch: "Merch",
+    "manage-site": "Manage Site",
+    artists: "Band Name",
+  }
+
+  let header;
+
+  for (let path in locations) {
+    if (location.pathname.includes(path)) {
+      header = locations[path]
+      break
+    }
+  }
+
+  if (!header) header = "Web Chord";
+
+  return <h1>{ header }</h1>
+}
 
 function Navigation() {
   return (
@@ -12,6 +34,10 @@ function Navigation() {
             className="nav-buttons"
           />
         </NavLink>
+      </div>
+
+      <div className="header">
+        <Header />
       </div>
 
       <ProfileButton />
