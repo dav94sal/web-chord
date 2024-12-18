db2e0795d4d334d0d3026f4797e0f8945361ea1c
david <dav94sal@gmail.com>
2024-12-18 15:04:42 -0600

    Add uniform time display

--- react-app/src/components/EditTourModal/EditShowModal.jsx
+++ react-app/src/components/EditTourModal/EditShowModal.jsx
@@ -3,34 +3,11 @@ import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { editShow } from "../../redux/tour";
 
-const constructDate = (show) => {
-    const datetime = new Date(show.datetime)
-    const year = datetime.getFullYear()
-    let day = datetime.getDate()
-    let month = datetime.getMonth() + 1
-
-    if (day < 10) day = `0${day}`
-    if (month < 10) month = `0${month}`
-
-    return `${year}-${month}-${day}`
-}
-
-const constructTime = (show) => {
-    const datetime = new Date(show.datetime)
-    let hours = datetime.getUTCHours();
-    let minutes = datetime.getUTCMinutes();
-
-    if (hours < 10) hours = `0${hours}`
-    if (minutes < 10) minutes = `0${minutes}`
-
-    return `${hours}:${minutes}`
-}
-
 function EditShowModal({ show }) {
     const [venue, setVenue] = useState(show.venue)
     const [city, setCity] = useState(show.city)
     const [state, setState] = useState(show.state)
-    const [headliners, setHeadliners] = useState(show.headliners)
+    const [openers, setOpeners] = useState(show.openers)
     const [date, setDate] = useState(constructDate(show))
     const [time, setTime] = useState(constructTime(show))
     const [errors, setErrors] = useState({})
@@ -38,7 +15,7 @@ function EditShowModal({ show }) {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        // console.log(date)
+        // console.log(show)
     })
 
     const handleSubmit = async e => {
@@ -51,59 +28,84 @@ function EditShowModal({ show }) {
             city,
             state,
             venue,
-            headliners,
+            openers,
             tour_id: show.tour_id
         }
         const response = await dispatch(editShow(newShow))
 
         if (response.errors) setErrors(response.errors)
-        else closeModal()
+            else closeModal()
     }
 
     return (
         <>
             <h2>Edit Show</h2>
             {errors.errors && <p>{errors.errors}</p>}
+            {errors.server && <p>{errors.server}</p>}
             <form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     value={venue}
                     onChange={(e) => setVenue(e.target.value)}
-                />
+                    />
                 <input
                     type="text"
                     value={city}
                     onChange={(e) => setCity(e.target.value)}
-                />
+                    />
                 <input
                     type="text"
                     value={state}
                     onChange={(e) => setState(e.target.value)}
-                />
+                    />
                 <input
                     type="text"
-                    value={headliners}
-                    onChange={(e) => setHeadliners(e.target.value)}
-                />
+                    value={openers}
+                    placeholder="Openers"
+                    onChange={(e) => setOpeners(e.target.value)}
+                    />
                 <input
                     type="date"
                     value={date}
                     onChange={(e) => setDate(e.target.value)}
-                />
+                    />
                 <input
                     type="time"
                     value={time}
                     onChange={(e) => setTime(e.target.value)}
-                />
+                    />
                 <button type="submit" className="buttons">Submit</button>
                 <button
                     type="click"
                     className="buttons"
                     onClick={() => closeModal()}
-                >Cancel</button>
+                    >Cancel</button>
             </form>
         </>
     )
+}
+
+function constructDate(show) {
+    const datetime = new Date(show.datetime)
+    const year = datetime.getFullYear()
+    let day = datetime.getDate()
+    let month = datetime.getMonth() + 1
+
+    if (day < 10) day = `0${day}`
+    if (month < 10) month = `0${month}`
+
+    return `${year}-${month}-${day}`
+}
+
+function constructTime(show) {
+    const datetime = new Date(show.datetime)
+    let hours = datetime.getUTCHours();
+    let minutes = datetime.getUTCMinutes();
+
+    if (hours < 10) hours = `0${hours}`
+    if (minutes < 10) minutes = `0${minutes}`
+
+    return `${hours}:${minutes}`
 }
 
 export default EditShowModal;
